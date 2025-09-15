97932425fcc29543d9adf97167dd6cf8042a7eb0
LeJays <125693939+LeJays@users.noreply.github.com>
2025-09-15 16:50:11 +0100

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,22 @@ import RecentTransactions from "./components/RecentTransactions"
 import StockSummaryTable from "./components/StockSummaryTable"
 
 export default function Home() {
-  const email = useCachedUser()
+  const cachedUser = useCachedUser() // c'est un objet MinimalUser | null
 
+  if (!cachedUser) {
+    return <div>Chargement...</div>
+  }
 
   return (
     <Wrapper>
       <div className="flex flex-col md:flex-row">
         <div className="md:w-2/3">
-          <ProductOverview email={email} />
-          <CategoryChart email={email} />
-          <RecentTransactions email={email} />
+          <ProductOverview email={cachedUser.email} />
+          <CategoryChart email={cachedUser.email} />
+          <RecentTransactions email={cachedUser.email} />
         </div>
         <div className="md:ml-4 md:mt-0 mt-4 md:w-1/3">
-          <StockSummaryTable email={email} />
+          <StockSummaryTable email={cachedUser.email} />
         </div>
       </div>
     </Wrapper>
